77c7a7a84f448d6c0122e752bf2809549bf1da37
Evgenij Ufandeev <46904677+Uffik81@users.noreply.github.com>
2021-01-14 18:24:33 +0300

    Create templare

---
+++ pywinkclient.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+
+# url = "https://cnt-vrzh-itv02.svc.iptv.rt.ru/api/v2/portal/channels?limit=30&offset=0&with_epg=true&epg_limit=3"
+# session_id = "016b36e6-567a-11eb-a007-9c1b36dcb53c:1951416:2237006:2"
+# TE = "Trailers"
+# X-Wink-Version = "v2020.12.30.1644"
+
+if __name__ == "__main__":
+    print("Wink client")
